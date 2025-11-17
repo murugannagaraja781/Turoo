18c9bd65d97e9a5cfa2e215097d8b8f2a6d3c543
murugannagaraja781 <murugannagaraja781@gmail.com>
2025-11-17 18:59:11 +0530

    ss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/turoo/", // Use your repo name EXACTLY
+  base: "/", // Use your repo name EXACTLY
 });
